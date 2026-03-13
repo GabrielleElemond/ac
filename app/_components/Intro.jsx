1be36193d3e4491c883df3a4d0c9a7042cd93a15
GabrielleElemond <0566134@cstj.qc.ca>
2026-03-12 22:46:19 -0400

    icons

--- app/_components/Intro.jsx
+++ app/_components/Intro.jsx
@@ -30,7 +30,7 @@ export default function () {
                 <video ref={video} className="video_intro" controls src="https://media.milanote.com/p/files/1W0QCk18CMAP0B/Gzs/intro_compressed.mp4" onEnded={pageSuivante}></video>
 
                 <div className="play" onClick={playVideo} style={boutonStyles}>
-                    <img src="narratriceSmirk.png" />
+                    <img src="play.png" />
                 </div>
             </div>
             <p className="zone_bt">
